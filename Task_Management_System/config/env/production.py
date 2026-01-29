9cc6be1424b2ea0b4586c9423fa77e08a7befde8
Shubham <shubhamjoshi9921@gmail.com>
2026-01-30 01:02:39 +0530

    Production setup: env switch, postgres config, cors, allowed hosts

--- Task_Management_System/config/env/production.py
+++ Task_Management_System/config/env/production.py
@@ -1,3 +1,31 @@
 from .base import *
-print('hi')
+import os
+
 DEBUG = False
+
+ALLOWED_HOSTS = [
+    "task.yourdesk.cloud",
+    "www.task.yourdesk.cloud",
+]
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "taskdb",
+        "USER": "taskuser",
+        "PASSWORD": "PrakharShreyans.CCPL@123",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://task.yourdesk.cloud",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://task.yourdesk.cloud",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
--- Task_Management_System/config/settings.py
+++ Task_Management_System/config/settings.py
@@ -1,3 +1,8 @@
-print(">>> SETTINGS.PY LOADED <<<")
-from .env.local import *
+import os
 
+ENV = os.getenv("DJANGO_ENV", "local")
+
+if ENV == "production":
+    from .env.production import *
+else:
+    from .env.local import *
